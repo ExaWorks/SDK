2e0dc3b20c85a4f5cc28758f0bd9e52bbb6661b5
Mike <mhategan@gmail.com>
2021-05-06 11:38:47 -0700

    Check the test output against expected output

---
+++ docker/Parsl/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+/test.py | tee /tmp/test-output
+
+cat <<EOF >/tmp/expected-output
+Hello World from Python!
+Hello World!
+
+EOF
+
+if diff /tmp/test-output /tmp/expected-output; then
+	echo "Output matches"
+else
+	echo "Output does not match"
+	exit 1
+fi
