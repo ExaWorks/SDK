e069072096fd7d805be8ff2a36492ebe3392e839
Andre Merzky <andre@merzky.net>
2021-05-14 17:21:43 +0200

    expand rp tests - add unit tests and component tests

--- docker/rp/run_rp_test.sh
+++ docker/rp/run_rp_test.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
 # run mongodb, let it settle
+echo '--- start MOngoDB'
 mongod --fork --logpath /tmp/mongodb.log
 
 cd radical.pilot
+echo '--- smoke test'
 ./examples/00_getting_started.py
-# pytest -vvv tests
+ret=$?
+echo "--- smoke test $ret"
 
+echo '--- unit test'
+pytest -vvv tests/unit_tests
+test "$ret" = 0 && ret=$?
+echo "--- unit test $ret"
+
+echo '--- component test'
+pytest -vvv tests/component_tests
+test "$ret" = 0 && ret=$?
+echo "--- component test $ret"
+
+exit $ret
 
