4c9dfb0b7877c4d4a35e09ee42b54b6f85c327d3
Stephen Herbein <stephen272@gmail.com>
2021-08-11 13:49:25 -0700

    flux: add job-shell plugin to fix pmi.clique
    Problem: When running with `flux start --test-size`, as the flux integration
    tests are, openmpi hangs. This is because when Flux is started with
    `--test-size`, the job-shell setting `pmi.clique` defaults to singleton, which
    means Flux's PMI tells OpenMPI that all ranks are on the same node, even when
    you use `flux mini run -N2`. This causes OpenMPI to hang.
    
    Solution: Add a job-shell plugin that forces Flux's PMI to "lie" to OpenMPI
    that each shell is running on a different node. OpenMPI properly bootstraps
    under this config.

--- docker/flux/test.sh
+++ docker/flux/test.sh
@@ -5,6 +5,22 @@ if [[ "$output" != "Success" ]]; then
     exit 1
 fi
 
+# When running with `flux start --test-size`, as the flux integration tests are,
+# openmpi hangs. This is because when Flux is started with `--test-size`, the
+# job-shell setting `pmi.clique` defaults to singleton, which means Flux's PMI
+# tells OpenMPI that all ranks are on the same node, even when you use `flux mini
+# run -N2`. This causes OpenMPI to hang. To solve this, add a job-shell plugin
+# that forces Flux's PMI to "lie" to OpenMPI that each shell is running on a
+# different node. OpenMPI properly bootstraps under this config.
+FLUX_LUA_D_DIR="$(dirname $(flux start flux getattr conf.shell_initrc))/lua.d"
+SHELL_PLUGIN_PATH="$FLUX_LUA_D_DIR/pmi-clique.lua"
+cat << EOF > $SHELL_PLUGIN_PATH
+pmiopt = shell.options.pmi or {}
+pmiopt["clique"] = "pershell"
+shell.options.pmi = pmiopt
+EOF
+chmod +x $SHELL_PLUGIN_PATH
+
 cd /tmp/
 git clone https://github.com/flux-framework/flux-core.git
 cd flux-core
@@ -23,6 +39,8 @@ exit_code=$?
 if [[ $exit_code -gt 0 ]]; then
     bash /checks-annotate.sh
 fi
+
+rm $SHELL_PLUGIN_PATH
 
 echo "Exiting with $exit_code"
 exit $exit_code
