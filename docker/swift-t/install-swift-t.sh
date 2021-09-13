df7547a7b51706e5a1b063e1916bea85edfee4cb
Mikhail Titov <mikhail.a.titov@gmail.com>
2021-09-13 14:58:40 -0400

    swift/t: resolved installation process by @j-woz

--- docker/swift-t/install-swift-t.sh
+++ docker/swift-t/install-swift-t.sh
@@ -13,8 +13,7 @@ PATH=/opt/tcl-8.6.11/bin:$PATH
 git clone https://github.com/swift-lang/swift-t.git
 
 # Build it
-pushd swift-t/dev/build
+cd swift-t/dev/build
 cp -v swift-t-settings.sh.template swift-t-settings.sh
 sed -i "/SWIFT_T_PREFIX=/s@=.*@=$SWIFT_ROOT@" swift-t-settings.sh
 ./build-swift-t.sh
-popd
