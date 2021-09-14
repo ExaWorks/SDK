6a4dfc3a91803924f631c04c9e6566ce145b86c8
Zeke Morton <morton30@llnl.gov>
2021-09-14 13:49:03 -0700

    swift-t: removed check for `tclsh` in deps

--- docker/swift-t/install-deps.sh
+++ docker/swift-t/install-deps.sh
@@ -4,14 +4,6 @@ set -eux
 # INSTALL DEPENDENCIES
 # Currently just Tcl
 
-if which tclsh8.6 > /dev/null 2>&1
-then
-  # Tcl is installed, probably via package manager.
-  # Note that this does not check for tcl-devel,
-  #      which is needed for Swift/T.
-  exit
-fi
-
 # Get Tcl
 wget --no-verbose https://prdownloads.sourceforge.net/tcl/tcl8.6.11-src.tar.gz
 tar xfz tcl8.6.11-src.tar.gz
