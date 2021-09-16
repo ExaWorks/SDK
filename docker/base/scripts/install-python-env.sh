012875f3da80565a66f93fb4855b856fe162667f
Zeke Morton <morton30@llnl.gov>
2021-09-16 11:37:29 -0700

    python-env: moved python build from dockerfile to script

---
+++ docker/base/scripts/install-python-env.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    echo "Must provide Package Manager that you want to install" 1>&2
+    exit 1
+fi
+
+<<<<<<< HEAD
+function centos_major_version() {
+    cat /etc/centos-release | cut -f 4 -d " " | cut -f 1 -d .
+}
+
+if [[ "$1" == "conda" ]]; then
+    if [[ $(centos_major_version) == "7" ]]; then
+        curl -sSL https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o /tmp/miniconda.sh
+        bash /tmp/miniconda.sh -bfp /usr/local/
+        rm -rf /tmp/miniconda.sh
+        conda create -y -p ${VIRTUAL_ENV} python=3.6
+        conda update conda
+        conda clean --all --yes
+        conda install -y -p $VIRTUAL_ENV pip setuptools pytest
+        # Flux python deps
+        conda install -y -p $VIRTUAL_ENV cffi six pyyaml jsonschema
+        # Parsl python deps
+        conda install -y -p $VIRTUAL_ENV "cryptography==3.3.2"
+
+    else
+        echo "Unknown CentOS version. Exiting" 1>&2
+        exit 1
+    fi
+
+elif [[ "$1" == "pip" ]]; then
+  python3 -m venv ${VIRTUAL_ENV}
+  pip install --upgrade pip setuptools pytest
+  # Flux python deps
+  pip install cffi six pyyaml jsonschema
+  # Parsl python deps
+  pip install "cryptography==3.3.2"
+=======
+# Get distribution info
+if [[ -f "/etc/centos-release" ]]; then
+    DISTRO_ID="centos"
+    DISTRO_MAJOR_VERSION=$(cat /etc/centos-release | cut -f 4 -d " " | cut -f 1 -d .)
+elif [[ -f "/etc/lsb-release" ]]; then
+    DISTRO_ID=$(cat /etc/lsb-release | grep DISTRIB_ID | cut -f 2 -d "=" | awk '{print tolower($0)}')
+    DISTRO_MAJOR_VERSION=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | cut -f 2 -d "=" | cut -f 1 -d .)
+else
+    echo "Unknown Linux distro. Exiting" 1>&2
+    exit 1
+fi
+
+if [[ "$1" == "conda" ]]; then
+    if [[ ${DISTRO_ID} == "centos" ]] && [[ ${DISTRO_MAJOR_VERSION} == "8" ]]; then
+        echo "Unsupported: Centos8 + Conda" 1>&2
+        exit 1
+    fi
+
+    curl -sSL https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o /tmp/miniconda.sh
+    bash /tmp/miniconda.sh -bfp /usr/local/
+    rm -rf /tmp/miniconda.sh
+    conda create -y -p ${VIRTUAL_ENV} python=3.6
+    conda update conda
+    conda clean --all --yes
+    conda install -y -p $VIRTUAL_ENV pip setuptools pytest
+    # Flux python deps
+    conda install -y -p $VIRTUAL_ENV cffi six pyyaml jsonschema
+    # Parsl python deps
+    conda install -y -p $VIRTUAL_ENV "cryptography==3.3.2"
+
+
+elif [[ "$1" == "pip" ]]; then
+    python3 -m venv ${VIRTUAL_ENV}
+    pip install --upgrade pip setuptools pytest
+    # Flux python deps
+    pip install cffi six pyyaml jsonschema
+    # Parsl python deps
+    pip install "cryptography==3.3.2"
+>>>>>>> Rebased from master
+
+else
+    printf "Unknown/unsupported Python Package Manager '%s'. Exiting without installing.\n" "$1"
+    exit 1
+fi
