6c0d38f1dbc0695bb1bdb3a7627a07b5ceeccd46
Zeke Morton <morton30@llnl.gov>
2021-12-28 12:46:58 -0800

    ci: added in spack integration tests for flux+rp and flux+parsl

---
+++ ci/tests/rp-flux/test.sh
@@ -0,0 +1,12 @@
+git clone -b master --single-branch https://github.com/radical-cybertools/radical.pilot.git
+
+mkdir -p ~/.radical/pilot/configs/
+cp resource_flux.json ~/.radical/pilot/configs/resource_flux.json
+
+cd radical.pilot
+echo '--- smoke test'
+./examples/00_getting_started.py flux.localhost_flux
+ret=$?
+echo "--- smoke test $ret"
+
+exit $ret
