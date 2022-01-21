3a26be7ae1536c2bfe994bded483c2964535cc0d
Zeke Morton <morton30@llnl.gov>
2022-01-21 08:17:38 -0700

    base: changed mpi env to mpi_flavor and removed -devel

--- docker/swift-t/test-sanity.sh
+++ docker/swift-t/test-sanity.sh
@@ -5,7 +5,7 @@ set -eux
 # Simply sanity tests for fresh Docker build
 
 # For OpenMPI
-if [[ $MPI == "openmpi-devel" ]]; then
+if [[ $MPI_FLAVOR == "openmpi" ]]; then
   export TURBINE_LAUNCH_OPTIONS=--allow-run-as-root
 fi
 
