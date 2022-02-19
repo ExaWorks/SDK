74275845e3f1b2ee8d281728480c33a7fb622937
Andre Merzky <andre@merzky.net>
2022-02-20 00:58:49 +0100

    remove url

--- ci/tests/rp/test.sh
+++ ci/tests/rp/test.sh
@@ -1,7 +1,5 @@
 git clone -b master --single-branch https://github.com/radical-cybertools/radical.pilot.git
 
-export RADICAL_PILOT_DBURL='mongodb://am:W6gvSzpGBqAeuR4Z@95.217.193.116:27017/am?tlsAllowInvalidCertificates=true'
-
 cd radical.pilot
 echo '--- smoke test'
 ./examples/00_getting_started.py
