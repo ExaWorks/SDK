e9f90a1e76625df4a441ff71ceeb671c88dbf683
Mikhail Titov <mikhail.a.titov@gmail.com>
2023-01-20 18:39:58 -0500

    tutorials: fixed builder

--- docker/tutorials/push-multi-platform.sh
+++ docker/tutorials/push-multi-platform.sh
@@ -3,6 +3,7 @@
 SDK_BASE_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )/../../" &> /dev/null && pwd 2> /dev/null; )"
 
 # push to the DockerHub registry
+docker buildx create --use --name sdk_builder
 docker buildx build \
     --output=type=registry \
     --platform linux/amd64,linux/arm64 \
