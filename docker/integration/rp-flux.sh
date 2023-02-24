f735ca5f1011cc731e3f9020d5897dd637087d68
Mikhail Titov <mikhail.a.titov@gmail.com>
2023-02-24 14:28:33 -0500

    sdk: added rp-flux testing

---
+++ docker/integration/rp-flux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if which radical-pilot-version >/dev/null; then
+    rp_version="$(radical-pilot-version)"
+    if [[ -z $rp_version ]]; then
+        echo "RADICAL-Pilot version unknown"
+        exit 1
+    fi
+else
+    echo "RADICAL-Pilot not installed"
+    exit 1
+fi
+
+git clone -b v$rp_version --single-branch https://github.com/radical-cybertools/radical.pilot.git
+cd radical.pilot
+
+echo '--- smoke test'
+./examples/00_getting_started.py 'flux.localhost_flux'
+ret=$?
+echo "--- smoke test $ret"
+
+test "$ret" = 0 && echo "Success!"
+exit $ret
