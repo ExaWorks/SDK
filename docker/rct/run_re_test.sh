4c6d202fef553e585c0975b77b17f3606b75860b
Mikhail Titov <mikhail.a.titov@gmail.com>
2023-02-24 12:54:51 -0500

    docker-rct: updated RCT (removed RMQ dependency)

--- docker/rct/run_re_test.sh
+++ docker/rct/run_re_test.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-export RMQ_HOSTNAME="rabbitmq"
-export RMQ_PORT=5672
-export RMQ_USERNAME="guest"
-export RMQ_PASSWORD="guest"
-
 # run mongodb, let it settle
 echo '--- start MongoDB'
 if [[ -f "/etc/mongod.conf" ]]; then
