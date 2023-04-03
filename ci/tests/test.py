54ffd1ab6e699e8b42d90f1e8ca66f046dc64639
Ramon Adolfo Arambula <ramon@quartz188.llnl.gov>
2023-04-03 12:56:20 -0700

    Renamed CustomHttpadapter() to TransferAdapter()

--- ci/tests/test.py
+++ ci/tests/test.py
@@ -34,7 +34,7 @@ data = { "run_id" : run_id,
         }
 
 
-class CustomHttpAdapter (requests.adapters.HTTPAdapter):
+class TransferAdapter(requests.adapters.HTTPAdapter):
     # "Transport adapter" that allows us to use custom ssl_context.
     def __init__(self, ssl_context=None, **kwargs):
         self.ssl_context = ssl_context
@@ -51,7 +51,7 @@ class CustomHttpAdapter (requests.adapters.HTTPAdapter):
         ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
         ctx.check_hostname = False
         session = requests.session()
-        session.mount('https://', CustomHttpAdapter(ctx))
+        session.mount('https://', TransferAdapter(ctx))
         return session
 
 
