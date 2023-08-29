1938139dd1d6a90c34faf293b73747af1ac071f1
Mikhail Titov <mikhail.a.titov@gmail.com>
2023-08-28 21:27:04 -0400

    ci-rp: fixed RP tests

--- ci/tests/rp-flux/test.sh
+++ ci/tests/rp-flux/test.sh
@@ -16,19 +16,17 @@ mkdir -p .radical/pilot/configs
 cp ci/tests/rp-flux/resource_flux.json .radical/pilot/configs/resource_flux.json
 export RADICAL_CONFIG_USER_DIR=$(pwd)
 
-if [[ ! -d "./radical.pilot" ]]; then
-    git clone -b v$rp_version --single-branch \
-    https://github.com/radical-cybertools/radical.pilot.git
-fi
-cd radical.pilot
+wget "https://raw.githubusercontent.com/radical-cybertools/radical.pilot/v$rp_version/examples/00_getting_started.py"
 
-echo '--- smoke test'
-./examples/00_getting_started.py 'flux.localhost_flux'
+radical-stack
+./00_getting_started.py 'flux.localhost_flux'
 ret=$?
 echo "--- smoke test $ret"
 
-rm -rf $HOME/radical.pilot.sandbox/*
+SID=$(ls -rt | grep rp.session)
+test -z "$SID" || rm -rf "$HOME/radical.pilot.sandbox/$SID"
 echo '--- cleaned pilot sandbox'
 
 test "$ret" = 0 && echo "Success!"
 exit $ret
+
--- ci/tests/rp/test.sh
+++ ci/tests/rp/test.sh
@@ -22,31 +22,15 @@ cat > .radical/pilot/configs/resource_local.json <<EOF
 EOF
 export RADICAL_CONFIG_USER_DIR=$(pwd)
 
-if [[ ! -d "./radical.pilot" ]]; then
-    git clone -b v$rp_version --single-branch \
-    https://github.com/radical-cybertools/radical.pilot.git
-fi
-cd radical.pilot
+wget "https://raw.githubusercontent.com/radical-cybertools/radical.pilot/v$rp_version/examples/00_getting_started.py"
 
-echo '--- smoke test'
-./examples/00_getting_started.py local.localhost
+radical-stack
+./00_getting_started.py local.localhost
 ret=$?
 echo "--- smoke test $ret"
 
 SID=$(ls -rt | grep rp.session)
 test -z "$SID" || rm -rf "$HOME/radical.pilot.sandbox/$SID"
-
-echo '--- unit test'
-pytest -vvv tests/unit_tests
-test "$ret" = 0 && ret=$?
-echo "--- unit test $ret"
-
-echo '--- component test'
-pytest -vvv tests/component_tests
-test "$ret" = 0 && ret=$?
-echo "--- component test $ret"
-
-rm -rf $HOME/radical.pilot.sandbox/*
 echo '--- cleaned pilot sandbox'
 
 test "$ret" = 0 && echo "Success!"
